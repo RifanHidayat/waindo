aa3300ee2ad6e3d1952eb2b93b83ee093631ca3a
Boyahawks <bayuhardiansyah73@gmail.com>
2022-11-07 08:43:01 +0700

    delete app tracking transparancy

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/mobile/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/mobile/Documents/mobile/hris_manajemen"
+export "FLUTTER_ROOT=C:\src\flutter"
+export "FLUTTER_APPLICATION_PATH=D:\data-bayu\siscom-mobile\siscom_operasional"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NAME=1.0.1"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
